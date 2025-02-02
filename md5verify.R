f71b8714406e5e7ddeff9b4366ceec484ef36925
Pawel Zayakin <pz@magisters.net>
2025-02-02 23:53:43 +0000

    enchance output

--- md5verify.R
+++ md5verify.R
@@ -5,18 +5,18 @@ system(paste0("cd /wd/ && (find /wd/ -name md5sum.txt; find /wd/ -name *.md5) > 
 files<-readLines(paste0("/wd/md5files.txt"))
 files<-files[!grepl("/[@#]",files)]
 f<-files[1]
-for(f in files){
-  print(f)
+for(i in 1:length(files)){
+  f<-files[i]
+  print(paste0(f," ",i,"/",length(files)))
   out<- sub(".md5$","_md5_check_report_NAS.txt",sub(".txt$","_md5_check_report_NAS.txt",f) )
   if(!file.exists(out)){
-      system(paste0("cd ",dirname(f)," && md5sum -c ",basename(f)," > ",out ),intern=TRUE)
+    print("Check...")
+    system(paste0("cd ",dirname(f)," && md5sum -c ",basename(f)," > ",out ),intern=TRUE)
   } else if(sub(" .*","",system(paste("wc -l",f),intern=TRUE)) != sub(" .*","",system(paste("wc -l",out),intern=TRUE))){
     # readLines(out)
+    print("Recheck...")
     system(paste0("cd ",dirname(f)," && md5sum -c ",basename(f)," > ",out ),intern=TRUE)
   }
-    
 }
-
 system(paste0("cd /wd/ && find /wd/ -name *_md5_check_report_NAS.txt -exec grep -v ' OK$' {} \\; > /wd/md5errors.txt"),intern=TRUE)
-
 print("Done")
