52a383e69be0c433321fb342af0cad5c336355e4
Pawel Zayakin <pz@magisters.net>
2025-02-03 00:09:51 +0000

    Enchance output

--- md5verify.R
+++ md5verify.R
@@ -5,18 +5,22 @@ system(paste0("cd /wd/ && (find /wd/ -name md5sum.txt; find /wd/ -name *.md5) > 
 files<-readLines(paste0("/wd/md5files.txt"))
 files<-files[!grepl("/[@#]",files)]
 f<-files[1]
+if(file.exists("/wd/md5verify.log")) unlink(("/wd/md5verify.log"))
 for(i in 1:length(files)){
   f<-files[i]
+  cat(paste0(f," ",i,"/",length(files)),file="/wd/md5verify.log",append = T)
   print(paste0(f," ",i,"/",length(files)))
   out<- sub(".md5$","_md5_check_report_NAS.txt",sub(".txt$","_md5_check_report_NAS.txt",f) )
   if(!file.exists(out)){
+    cat("Check...",file="/wd/md5verify.log",append = T)
     print("Check...")
     system(paste0("cd ",dirname(f)," && md5sum -c ",basename(f)," > ",out ),intern=TRUE)
   } else if(sub(" .*","",system(paste("wc -l",f),intern=TRUE)) != sub(" .*","",system(paste("wc -l",out),intern=TRUE))){
     # readLines(out)
+    cat("Recheck...",file="/wd/md5verify.log",append = T)
     print("Recheck...")
     system(paste0("cd ",dirname(f)," && md5sum -c ",basename(f)," > ",out ),intern=TRUE)
   }
 }
-system(paste0("cd /wd/ && find /wd/ -name *_md5_check_report_NAS.txt -exec grep -v ' OK$' {} \\; > /wd/md5errors.txt"),intern=TRUE)
+system(paste0("cd /wd/ && find /wd/ -name *_md5_check_report_NAS.txt -exec grep --with-filename -v ' OK$' {} \\; > /wd/md5errors.txt && sed -i 's!^/wd/!!g' /wd/md5errors.txt"),intern=TRUE)
 print("Done")
