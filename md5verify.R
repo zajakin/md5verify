b489a43cfe6e109a3eb48915eda7dff6a107dfd5
Pawel Zayakin <pz@magisters.net>
2025-02-03 00:49:58 +0000

    stderr to logs

--- md5verify.R
+++ md5verify.R
@@ -1,12 +1,12 @@
 #!/usr/bin//usr/bin/Rscript --vanilla
 # wd <- "/wd/"
 # if(!grepl("/$",wd)) wd<-paste0(wd,"/")
-system(paste0("cd /wd/ && (find /wd/ -name md5sum.txt; find /wd/ -name *.md5) > /wd/md5files.txt"),intern=TRUE)
+if(file.exists("/wd/md5verify.log")) unlink(("/wd/md5verify.log"))
+cat(date(),file="/wd/md5verify.log",append = T)
+system(paste0("cd /wd/ && (find /wd/ -name md5sum.txt; find /wd/ -name *.md5) > /wd/md5files.txt 2> /wd/md5verify.log"),intern=TRUE)
 files<-readLines(paste0("/wd/md5files.txt"))
 files<-files[!grepl("/[@#]",files)]
 f<-files[1]
-if(file.exists("/wd/md5verify.log")) unlink(("/wd/md5verify.log"))
-cat(date(),file="/wd/md5verify.log",append = T)
 for(i in 1:length(files)){
   f<-files[i]
   cat(paste0("\n",f," ",i,"/",length(files)),file="/wd/md5verify.log",append = T)
@@ -23,6 +23,6 @@ for(i in 1:length(files)){
     system(paste0("cd ",dirname(f)," && md5sum -c ",basename(f)," > ",out," 2>&1"),intern=TRUE)
   }
 }
-system(paste0("cd /wd/ && find /wd/ -name *_md5_check_report_NAS.txt -exec grep --with-filename -v ' OK$' {} \\; > /wd/md5errors.txt && sed -i 's!^/wd/!!g' /wd/md5errors.txt"),intern=TRUE)
+system(paste0("cd /wd/ && find /wd/ -name *_md5_check_report_NAS.txt -exec grep --with-filename -v ' OK$' {} \\; > /wd/md5errors.txt 2>&1 && sed -i 's!^/wd/!!g' /wd/md5errors.txt"),intern=TRUE)
 cat(paste0("\n",date()),file="/wd/md5verify.log",append = T)
 print("Done")
