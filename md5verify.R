773a742be25c867ffcc27c49450256bc485b174b
Pawel Zayakin <pz@magisters.net>
2025-12-11 17:05:03 +0000

    not_covered_by_md5

--- md5verify.R
+++ md5verify.R
@@ -3,8 +3,15 @@
   if(file.exists("md5verify.log")) unlink(("md5verify.log"))
   cat(date(),file="md5verify.log",append = T)
   system(paste0("(find -not -path '*/#recycle/*' -name md5sum.txt; find -not -path '*/#recycle/*' -name *.md5; find -not -path '*/#recycle/*' -name *_FileInfo.txt) > md5files.txt 2> md5verify.log"),intern=TRUE)
+  files<-system(paste0("(find -not -path '*/#recycle/*' -name *q.gz; find -not -path '*/#recycle/*' -name *.bam; find -not -path '*/#recycle/*' -name *.pod5)"),intern=TRUE)
+  files<-files[!grepl("/[@#]",files)]
+  md5<-readLines("md5files.txt")
+  md5<-md5[!grepl("/[@#]",md5)]
+  for(i in md5) if(file.exists(i) && file.size(i)>0) files<-files[!files %in% file.path(dirname(i),sub("^./","",as.data.frame(do.call(rbind, strsplit(readLines(i), "  ", fixed = TRUE)))[,2]))]
+  cat(files,file="not_covered_by_md5.txt",append = F,sep = "\n")
   system(paste0("find -not -path '*/#recycle/*' -name *.pod5 > pod5files.txt"),intern=TRUE)
-  Sys.chmod(c("md5verify.log","md5files.txt","pod5files.txt"), mode = "0666")
+  system(paste0("df -h /wd | awk  '/wd/ {print $4}' > space"),intern=TRUE)
+  Sys.chmod(c("not_covered_by_md5.txt","md5verify.log","md5files.txt","pod5files.txt","space"), mode = "0666")
   files<-readLines(paste0("md5files.txt"))
   files<-files[!grepl("/[@#]",files)]
   i<-1
--- not_covered_by_md5.R
+++ not_covered_by_md5.R
@@ -1,17 +1,16 @@
 #!/usr/bin//usr/bin/Rscript --vanilla
   setwd("/wd/")
-  system(paste0("(find -not -path '*/#recycle/*' -name *q.gz; find -not -path '*/#recycle/*' -name *.bam; find -not -path '*/#recycle/*' -name *.pod5) > seqfiles.txt"),intern=TRUE)
-  files<-readLines("seqfiles.txt")
+  files<-system(paste0("(find -not -path '*/#recycle/*' -name *q.gz; find -not -path '*/#recycle/*' -name *.bam; find -not -path '*/#recycle/*' -name *.pod5)"),intern=TRUE)
   files<-files[!grepl("/[@#]",files)]
-  system(paste0("(find -not -path '*/#recycle/*' -name md5sum.txt; find -not -path '*/#recycle/*' -name *.md5; find -not -path '*/#recycle/*' -name *_FileInfo.txt) > md5files.txt"),intern=TRUE)
-  md5<-readLines("md5files.txt")
+  md5<-system(paste0("find -not -path '*/#recycle/*' -name md5sum.txt; find -not -path '*/#recycle/*' -name *.md5; find -not -path '*/#recycle/*' -name *_FileInfo.txt"),intern=TRUE)
+  # md5<-readLines("md5files.txt")
   md5<-md5[!grepl("/[@#]",md5)]
-  md5<-gsub("^/wd",".",md5)
+  # md5<-gsub("^/wd",".",md5)
   for(i in md5) if(file.exists(i) && file.size(i)>0) files<-files[!files %in% file.path(dirname(i),sub("^./","",as.data.frame(do.call(rbind, strsplit(readLines(i), "  ", fixed = TRUE)))[,2]))]
   cat(files,file="not_covered_by_md5.txt",append = F,sep = "\n")
-  Sys.chmod(c("not_covered_by_md5.txt","seqfiles.txt","md5files.txt"), mode = "0666")
+  Sys.chmod(c("not_covered_by_md5.txt","md5files.txt"), mode = "0666")
 
-  for(i in unique(sub("/.*","",sub("^./","",files)))){
+  for(i in unique(sub("/.*","",sub("^./","",files)))){ # tar: Auto_user_SN2-20-IJ-2-11-12-13-14-16042014_44_015: file changed as we read it
     # system(paste0("cd '",i,"' && md5sum `find -type f \\( -not -path '*/#recycle/*' -not -name md5sum.txt -not -name NAS_generated.md5 -not -name *_md5_check_report_NAS.txt \\)` > NAS_generated.md5"),intern=TRUE)
     system(paste0("cd '",i,"' && find -type f -not -name NAS_generated.md5 -not -name *_md5_check_report_NAS.txt -exec md5sum '{}' \\; > NAS_generated.md5"),intern=TRUE)
     # system(paste0("cd '",i,"' && find -type f -not -name NAS_generated.md5 -not -name *_md5_check_report_NAS.txt -exec md5sum '{}' \\; > NAS_generated.md5 && cd .. && tar cf - '",i,"' | gzip -f9 > '",i,"'.tar.gz"),intern=TRUE)
@@ -19,8 +18,8 @@
   # docker pull ghcr.io/zajakin/md5verify && docker run --rm -it --name=md5cover -v /volume3/from_MGI:/wd ghcr.io/zajakin/md5verify /usr/bin/Rscript --vanilla /not_covered_by_md5.R
   # find -type f \( -not -name NAS_generated.md5 -not -name *_md5_check_report_NAS.txt \) -exec md5sum '{}' \; > NAS_generated.md5
   
-  system(paste0("find -type f -not -path '*/#recycle/*' -not -name *q.gz -not -name *.bam -not -name *.pod5 > notseqfiles.txt"),intern=TRUE)
-  files<-readLines("notseqfiles.txt")
+  files<-system(paste0("find -type f -not -path '*/#recycle/*' -not -name *q.gz -not -name *.bam -not -name *.pod5"),intern=TRUE)
+  # files<-readLines("notseqfiles.txt")
   files<-files[!grepl("/[@#]",files)]
   for(i in md5) if(file.size(i)>0) files<-files[!files %in% file.path(dirname(i),sub("^./","",as.data.frame(do.call(rbind, strsplit(readLines(i), "  ", fixed = TRUE)))[,2]))]
   cat(files,file="not_covered_by_md5_notseqfiles.txt",append = F,sep = "\n")
